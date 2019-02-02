39d08e21256fb05934b6c4b5a326750051281681
luolei <luoleicn@gmail.com>
2019-02-02 15:32:23 +0000

    add corpus

---
+++ corpus/download.sh
@@ -0,0 +1,2 @@
+curl http://kdd.ics.uci.edu/databases/20newsgroups/mini_newsgroups.tar.gz > mini_newsgroups.tar.gz
+curl http://kdd.ics.uci.edu/databases/20newsgroups/20_newsgroups.tar.gz > 20_newsgroups.tar.gz
